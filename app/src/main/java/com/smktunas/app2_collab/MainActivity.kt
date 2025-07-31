5b12ea90ecd12991e0e02fe2cc84d380743726a7
ramaadityaf <ramaadityaf29@gmail.com>
2025-07-31 08:43:34 +0700

    logic commit

--- app/src/main/java/com/smktunas/app2_collab/MainActivity.kt
+++ app/src/main/java/com/smktunas/app2_collab/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.smktunas.app2_collab
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val inputNama = findViewById<EditText>(R.id.etNama)
+        val inputKelas = findViewById<EditText>(R.id.etKelas)
+        val tvTampil = findViewById<TextView>(R.id.tvHasil)
+        val btnTampil = findViewById<Button>(R.id.btnTampilkan)
+
+        btnTampil.setOnClickListener {
+            val Nama = inputNama.text.toString()
+            val Kelas = inputKelas.text.toString()
+            val hasil = "Nama : ${Nama}\n Kelas : ${Kelas}"
+            tvTampil.text = hasil
         }
     }
 }
